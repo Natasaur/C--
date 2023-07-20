a60deeb6497571e7aed11a81c41854e04ef941b8
Natasaur <gatoslibres2231@gmail.com>
2023-07-20 09:18:18 -0600

    Clase 20/07/23

--- Hola_Mundo.cpp
+++ Hola_Mundo.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
+#include<iostream> //Entrada de datos y salida en monitor
 
+using namespace std; //Todos los elementos que entren al programa son leídos por la librería iostream
+
+//Esto es un comentario :3
+int a; //Declaración global
 int main(int argc, const char** argv) {
-    std::cout << "Hola Mundo!" << std::endl;
+    int b; //Declaración local
+    a=9;
+    cout << "Hola Mundo! " << a;
+
+
     return 0;
 }
