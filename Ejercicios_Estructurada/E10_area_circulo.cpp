5ebd5706eca24bbba62b5eb07bb16ee1618dba3b
Natasaur <gatoslibres2231@gmail.com>
2023-07-25 21:55:48 -0600

    Adelanto 20 ejercicios

---
+++ Ejercicios_Estructurada/E10_area_circulo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+/*Crear un algoritmo que lea el radio de un círculo y calcule el área.*/
+float r,a;
+int main()
+{
+    cout << "Ingrese el radio del círculo: ";
+    cin >> r;
+    a=M_PI*pow(r,2);
+    cout << "El área del círculo es: " << a;
+    
+    return 0;
+}
+
+
